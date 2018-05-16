a6ad844ee88c4c6d458989f1f672cd4b4c4d1d3c
朱宇斌 <651445686@qq.com>
2018-05-16 13:26:47 +0800

    update

--- ncmerge/src/test/java/NetCDFMergeTest.java
+++ ncmerge/src/test/java/NetCDFMergeTest.java
@@ -34,45 +34,6 @@ public class NetCDFMergeTest {
                 NetCDFMerge.dzdtMerge();
                 NetCDFMerge.pottmpMerge();
             }
-
-            NetcdfFile uflxNC = NetcdfFile.open("misc/GODAS/merged/uflx.nc");
-            NetcdfFile vflxNC = NetcdfFile.open("misc/GODAS/merged/vflx.nc");
-            Variable uflx = uflxNC.findVariable("uflx");
-            Variable vflx = vflxNC.findVariable("vflx");
-            Array uflxArray = uflx.read(new int[] {0, 0, 0}, new int[] {1, 418, 360});
-            Array vflxArray = vflx.read(new int[] {0, 0, 0}, new int[] {1, 418, 360});
-            uflxArray = uflxArray.reduce();
-            vflxArray = vflxArray.reduce();
-            JSONArray jsonArray = new JSONArray();
-            DecimalFormat decimalFormat = new DecimalFormat("0.00");
-            for (int lat = 0; lat < 418; lat += 1) {
-                for (int lon = 0; lon < 360; lon += 1) {
-                    float u = uflxArray.getFloat(lat * 360 + lon);
-                    float v = vflxArray.getFloat(lat * 360 + lon);
-                    if (u == 32767) {
-                        //u = 0;
-                    }
-                    if (v == 32767) {
-                        //v = 0;
-                    }
-                    JSONArray jsonArrayTemp = new JSONArray();
-                    jsonArrayTemp.put(Double.parseDouble(decimalFormat.format(-74.5 + lat / 3.0)));
-                    jsonArrayTemp.put(Double.parseDouble(decimalFormat.format(0.5 + lon)));
-                    jsonArrayTemp.put(u);
-                    jsonArrayTemp.put(v);
-                    jsonArrayTemp.put(Double.parseDouble(decimalFormat.format(sqrt(u * u + v * v))));
-                    jsonArray.put(jsonArrayTemp);
-                }
-            }
-            System.out.println(jsonArray.length());
-            System.out.println(uflxNC.getVariables());
-
-
-
-            PrintStream printStream = new PrintStream("flx.json");
-            System.setOut(printStream);
-            System.out.println(jsonArray.toString());
-
         }
         catch (IOException e) {
             e.printStackTrace();
